9d213b7e0c8cfb80556015944f9e835265d65520
kolvitamins <jkolvites@gmail.com>
2024-09-17 14:07:04 -0400

    90 - modify object add a new private field with appropriate accessors, mutators and reporting

--- Book.java
+++ Book.java
@@ -73,7 +73,14 @@ class Book
      */
     public void setRefNumber(String ref)
     {
-        refNumber = ref;
+        if(refNumber.length() > 3)
+        {
+            refNumber = ref;
+        }
+        else
+        {
+            System.out.println("Reference number must be at least 3 characters");
+        }
     }
     
     /**
@@ -95,7 +102,7 @@ class Book
         System.out.println("Title: " + title + ", Author: "
         + author + ", Pages: " + pages);
         
-        if (refNumber.length()>0)
+        if (refNumber.length() > 0)
         {
             System.out.println("Reference number: " + refNumber);
         }
